01992daa13428ee0fb3f63abdff209de2d40e490
digitalnp1988 <hamrodgtalnepal@gmail.com>
2026-05-06 19:02:19 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
-import { db, collection, getDocs } from "./firebase.js";
-
-async function loadProducts(){
-const querySnapshot = await getDocs(collection(db, "products"));
-
-let html = "";
-
-querySnapshot.forEach((doc) => {
-let p = doc.data();
-
-```
 html += `
 <div class="card">
+  <img src="${p.image}" style="width:100%; height:200px; object-fit:cover;">
   <h3>${p.name}</h3>
   <p>Rs. ${p.price}</p>
   <p>${p.desc}</p>
 </div>
 `;
-```
-
-});
-
-document.getElementById("products").innerHTML = html;
-}
-
-loadProducts();
