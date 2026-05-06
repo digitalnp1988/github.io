7a066b0a43b874f62612d109d69bc9ed0cbab86b
digitalnp1988 <hamrodgtalnepal@gmail.com>
2026-05-06 19:11:15 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 html += `
 <div class="card">
-  <img src="${p.image}" style="width:100%; height:200px; object-fit:cover;">
+  <img src="${p.image || 'https://via.placeholder.com/200'}" 
+       style="width:100%; height:200px; object-fit:cover;">
   <h3>${p.name}</h3>
   <p>Rs. ${p.price}</p>
   <p>${p.desc}</p>
